f76e341358b16ac38911093e20bfd7a4757f9a6c
Saito <saitowu@gmail.com>
2024-03-06 22:05:46 +1100

    add Next SEO

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,7 +1,19 @@
+import { useRouter } from 'next/router'
+import { useConfig } from 'nextra-theme-docs'
+
 export default {
   logo: <span>硬地骇客 - 2 个月 12000 美金的实践之路</span>,
   project: {
     link: "https://github.com/hardhackerlabs/book",
   },
+  docsRepositoryBase: 'https://github.com/hardhackerlabs/book/tree/main/',
+  useNextSeoProps() {
+    const { asPath } = useRouter()
+    const { frontMatter } = useConfig()
+
+    return {
+      titleTemplate: frontMatter.title + ' – 硬地骇客'
+    }
+  }
   // ... other theme options
 };
