be67e78f14c08209e8cb7753537e1d0f030c278d
Saito <saitowu@gmail.com>
2024-03-08 22:13:13 +1100

    rename title and fix some typo

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,19 +1,17 @@
-import { useRouter } from 'next/router'
-import { useConfig } from 'nextra-theme-docs'
+import { useConfig } from "nextra-theme-docs";
 
 export default {
-  logo: <span>硬地骇客 - 2 个月 12000 美金的实践之路</span>,
+  logo: <span>硬地骇客 - 两个月 $12000 ARR 实践之路</span>,
   project: {
     link: "https://github.com/hardhackerlabs/book",
   },
-  docsRepositoryBase: 'https://github.com/hardhackerlabs/book/tree/main/',
+  docsRepositoryBase: "https://github.com/hardhackerlabs/book/tree/main/",
   useNextSeoProps() {
-    const { asPath } = useRouter()
-    const { frontMatter } = useConfig()
+    const { frontMatter } = useConfig();
 
     return {
-      titleTemplate: frontMatter.title + ' – 硬地骇客'
-    }
-  }
+      titleTemplate: frontMatter.title + " – 硬地骇客",
+    };
+  },
   // ... other theme options
 };
