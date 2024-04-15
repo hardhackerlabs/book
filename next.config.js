8ac6c0f7240d3a4795724c101a088282f0dc8c11
Anson Chan <anson0370@gmail.com>
2024-04-15 23:32:29 +0800

    add assetPrefix for production env

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 const withNextra = require("nextra")({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.jsx",
 });
 
-module.exports = withNextra();
+module.exports = withNextra({
+  assetPrefix: isProd : 'https://book.hardhacker.com' : undefined,
+});
 
 // If you have other Next.js configurations, you can pass them as the parameter:
 // module.exports = withNextra({ /* other next.js config */ })
